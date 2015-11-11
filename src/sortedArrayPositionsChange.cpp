98f55e29c58ed0a7906af0457537ee95299b5932
srujanbelde <srujanbelde18@gmail.com>
2015-11-11 21:26:51 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -13,7 +13,27 @@ NOTES:
 
 #include <stdio.h>
 
-void * sortedArrayPositionsChange(int *Arr, int len)
+void * sortedArrayPositionsChange(int *arry, int len)
 {
-	return NULL;
+	int i, position[2] = { 0, 0 }, j, lesscount = 0, k = -1, temp;
+	if (arry && len>0){
+		for (i = 0; i < len; i++)
+		{
+			for (j = 0; j < len; j++)
+			{
+				if (arry[j] < arry[i])
+					lesscount++;
+			}
+			if (lesscount != i)
+			{
+				position[++k] = i;
+			}
+			lesscount = 0;
+		}
+		temp = arry[position[0]];
+		arry[position[0]] = arry[position[1]];
+		arry[position[1]] = temp;
+	}
+	else
+		return NULL;
 }
